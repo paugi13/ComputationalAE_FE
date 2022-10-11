443611f8abc1dcd560a0aedd67f9b9b53ccb588d
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-10-11 17:30:15 +0200

    processs made automatic

--- A1/Compute_Fe_Force.m
+++ A1/Compute_Fe_Force.m
@@ -8,16 +8,20 @@ N = 0.5*N;
 
 fPh = zeros(size(N,1),1);
 
+sum = 0;
 for i=1:size(N,1)
     xPh = N(i,:)*wcoord;
     fPh(i,1) = subst(f, x, xPh);
+    sum = sum + w*N(i,:).'*fPh(i);
 end
+
+Fe = he/2*sum;
 
 % xPh1 = N(1,:)*wcoord;
 % f1 = subst(f, x, xPh1);
 % xPh2 = N(1,:)*wcoord;
 % f2 = subst(f, x, xPh2);
 
-Fe = he/2*(w*N(1,:).'*fPh(1)+w*N(2,:).'*fPh(2));
+% Fe = he/2*(w*N(1,:).'*fPh(1)+w*N(2,:).'*fPh(2));
 end
 
