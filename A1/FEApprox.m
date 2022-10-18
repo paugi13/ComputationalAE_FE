a930762f1f1cd19c4f839554c8477145c72d8cad
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-10-18 11:53:11 +0200

    added plotting settings

--- A1/FEApprox.m
+++ A1/FEApprox.m
@@ -23,19 +23,67 @@ errorDer(1, i) = sqrt(max(errorDerVector));
 end
 
 %% Post-process
-% plot multiple solutions
+% % plot multiple solutions
+% figure
+% hold on
+% plot(coord(1,1:(n(1)+1)), u(1,1:(n(1)+1)));
+% plot(coord(2,1:(n(2)+1)), u(2,1:(n(2)+1)));
+% plot(coord(3,1:(n(3)+1)), u(3,1:(n(3)+1)));
+% plot(coord(4,1:(n(4)+1)), u(4,1:(n(4)+1)));
+% xlabel('x (m)');
+% ylabel('u (m)');
+% title('Finite element approximations');
+% legend('nElem = 5', 'nElem = 10', 'nElem = 20', 'nElem = 40', 'location', 'northwest');
+% grid on
+% hold off
+
+load('exactSolExp.mat');
+% comparison with exact solution
 figure
+subplot(2,2,1);
 hold on
-plot(coord(1,1:(n(1)+1)), u(1,1:(n(1)+1)));
-plot(coord(2,1:(n(2)+1)), u(2,1:(n(2)+1)));
-plot(coord(3,1:(n(3)+1)), u(3,1:(n(3)+1)));
-plot(coord(4,1:(n(4)+1)), u(4,1:(n(4)+1)));
+fplot(exactSol, [0 1], 'b');
+plot(coord(1,1:(n(1)+1)), u(1,1:(n(1)+1)), 'r');
 xlabel('x (m)');
 ylabel('u (m)');
-title('Finite element approximations');
-legend('nElem = 5', 'nElem = 10', 'nElem = 20', 'nElem = 40', 'location', 'northwest');
 grid on
+title('nEl = 5');
+legend('Exact sol.', 'FE sol.', 'location', 'northwest');
 hold off
+
+subplot(2,2,2);
+hold on
+fplot(exactSol, [0 1], 'b');
+plot(coord(2,1:(n(2)+1)), u(2,1:(n(2)+1)), 'r');
+xlabel('x (m)');
+ylabel('u (m)');
+grid on
+title('nEl = 10');
+hold off
+
+subplot(2,2,3);
+hold on
+fplot(exactSol, [0 1], 'b');
+plot(coord(3,1:(n(3)+1)), u(3,1:(n(3)+1)), 'r');
+xlabel('x (m)');
+ylabel('u (m)');
+grid on
+title('nEl = 20');
+hold off
+
+subplot(2,2,4);
+hold on
+fplot(exactSol, [0 1], 'b');
+plot(coord(4,1:(n(4)+1)), u(4,1:(n(4)+1)), 'r');
+xlabel('x (m)');
+ylabel('u (m)');
+grid on
+title('nEl = 40');
+hold off
+
+
+
+
 
 
 % element size vector
@@ -51,10 +99,12 @@ figure
 plot(log(n), log(error), 'b');
 hold on
 plot(log(n), log(errorDer), 'r');
+% loglog(n, errorDer);
 xlabel('log(nElements)');
 ylabel('log(\epsilon_{max})');
 grid on
 title('Total error vs number of elements');
+legend('||e||', '||e''||');
 hold off
 
 
@@ -67,5 +117,6 @@ xlabel('log(elementSize)');
 ylabel('log(\epsilon_{max})');
 grid on
 title('Total error vs element size');
+legend('||e||', '||e''||', 'location', 'northwest');
 hold off
 
