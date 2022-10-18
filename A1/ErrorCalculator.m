a31c7da0a2c5f86e44105c912412cfed29ce7d5f
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-10-18 11:52:55 +0200

    Fixed N matrix derivative

--- A1/ErrorCalculator.m
+++ A1/ErrorCalculator.m
@@ -17,7 +17,7 @@ fPh = zeros(size(N,1),1);
 % Load exact symbolic solution
 syms x
 load('exactSolExp.mat');
-B = 0.5*[-1, 1];
+
 % error calculator
 for i=1:nelem
     wcoord = [coord(1,i); coord(1,(i+1))];
@@ -27,7 +27,7 @@ for i=1:nelem
     for j=1:size(N,1)
         xPh = N(j,:)*wcoord;
         fPh(j,1) = subs(exactSol, x, xPh);
-        sum = sum + (w*(fPh(j,1)-B*de))^2;
+        sum = sum + (w*(fPh(j,1)-N(j,:)*de))^2;
     end
     errorVector(i,1) = he/2*sum;
 end
@@ -39,12 +39,13 @@ derExactSol = diff(exactSol, x);
 for i=1:nelem
     wcoord = [coord(1,i); coord(1,(i+1))];
     he = wcoord(2) - wcoord(1);
+    B = 1/he*[-1, 1];
     sum = 0;
     de = [d(1,i); d(1,(i+1))];
     for j=1:size(N,1)
         xPh = N(j,:)*wcoord;
         fPh(j,1) = subs(derExactSol, x, xPh);
-        sum = sum + (w*(fPh(j,1)-N(j,:)*de))^2;
+        sum = sum + (w*(fPh(j,1)-B*de))^2;
     end
     errorDerVector(i,1) = he/2*sum;
 end
