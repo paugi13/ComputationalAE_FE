1554e60eef62e51956cb9eec7737f47f38dfbf3f
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-10-31 10:44:00 +0100

    implemented two gauss point calculus

--- A1_Advanced/functions/AssemblyKnon.m
+++ A1_Advanced/functions/AssemblyKnon.m
@@ -13,12 +13,21 @@ for e=1:nelem  % Loop over number of elements
     Be = 1/he*[-1 1];
     % Young Modulus & Area Calculation
     epsilon_e = Be*d_k(NODOSe);
-    Ee = DerStressFUN(epsilon_e);
-    A1 = AreaFUN(COOR_e(1));
-    A2 = AreaFUN(COOR_e(2));
-    Ae = (A1+A2)/2;
+    Ee = DerStressFUN(epsilon_e);   
+    %-----------------------------------
+    xi1 = 1/sqrt(3);
+    xi2 = -1/sqrt(3);
+    N = zeros(2);
+    N(1, :) = [1-xi1, 1+xi1];
+    N(2, :) = [1-xi2, 1+xi2];
+    N = 0.5*N;
+    Asum = 0;
+    for i=1:size(N,1)
+        xPh = N(i,:)*COOR_e;
+        Asum = Asum + AreaFUN(xPh);
+    end
     % Elemental matrix
-    Ke = Ee*Ae/he*[1 -1; -1 1]; 
+    Ke = Ee/2*Asum/he*[1 -1; -1 1]; 
     % Assembly
     for a = 1:nnodeE
         for b = 1:nnodeE
